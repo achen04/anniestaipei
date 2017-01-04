1c5560fc76561327c04d5bc8b92d146ba8ae7b29
Annie Chen <annie.chen@tufts.edu>
2017-01-04 18:00:32 +0800

    center taipei

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 25.033, lng: 121.565},
+          zoom: 10
         });
       }
