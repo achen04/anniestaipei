76a07ba3f18bc3a455e72b04870da81aacd0f6e1
Annie Chen <annie.chen@tufts.edu>
2017-01-05 19:02:02 +0800

    no init

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-	initMap();
-});
+// $(document).ready(function() {
+// 	initMap();
+// });
 
 
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 25.033, lng: 121.565},
-          zoom: 5
+          zoom: 12
         });
       }
 
