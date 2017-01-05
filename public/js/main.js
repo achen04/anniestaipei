259883e034845c3e3029b3a9966cf73a1a9a63b9
Annie Chen <annie.chen@tufts.edu>
2017-01-05 19:07:57 +0800

    testing sample code

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,41 @@
 
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 25.033, lng: 121.565},
-          zoom: 12
-        });
-      }
+// var map;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 25.033, lng: 121.565},
+//           zoom: 12
+//         });
+//       }
+
+// // var markerPositionMountain = new google.maps.LatLng(25.093,121.571);
+// // var markerIconMoutain = {
+// // 	url: 'icons/MarkerMountain.png',
+// // 	size: new google.maps.Size(225, 120),
+// // 	origin: new google.maps.Point(0, 0),
+// // 	anchor: new google.maps.POint(189, 116)
+// // };
 
 // var markerPositionMountain = new google.maps.LatLng(25.093,121.571);
-// var markerIconMoutain = {
-// 	url: 'icons/MarkerMountain.png',
-// 	size: new google.maps.Size(225, 120),
-// 	origin: new google.maps.Point(0, 0),
-// 	anchor: new google.maps.POint(189, 116)
-// };
+// var marker = new google.maps.Marker({
+// 	position: markerPositionMountain,
+// 	map: map,
+// 	title: 'hello world!'
+// });
 
-var markerPositionMountain = new google.maps.LatLng(25.093,121.571);
-var marker = new google.maps.Marker({
-	position: markerPositionMountain,
-	map: map,
-	title: 'hello world!'
-});
+// // To add the marker to the map, call setMap()
 
-// To add the marker to the map, call setMap()
+
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
